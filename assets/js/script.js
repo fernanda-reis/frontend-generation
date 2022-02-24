1045164fb7ce3d5a8c04187377f8153fa4015829
Fernanda <fernandasoares.reis1@gmail.com>
2022-02-23 22:32:34 -0300

    personalização das páginas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,16 @@
-
-let nome = document.getElementById('nome')
+let nome = document.querySelector('#nome')
 let email = document.querySelector('#email')
-let assunto = document.querySelector('#assunto')
+let filme = document.querySelector('#melhorFilme')
 
 let nomeOk = false
 let emailOk = false
-let assuntoOk = false
-
-let mapa = document.querySelector('#mapa')
-
-nome.style.width = '100%'
-
-email.style.width = '100%'
+let filmeOk = false
 
 function validaNome() {
     let txtNome = document.querySelector('#txtNome')
     if (nome.value.length < 3) {
         txtNome.innerHTML = 'Nome inválido'
-        txtNome.style.color = 'red'
+        txtNome.style.color = 'pink'
         nomeOk = false
     } else {
         txtNome.innerHTML = ""
@@ -25,49 +18,36 @@ function validaNome() {
     }
 }
 
-function validaEmail(){
+function validaEmail() {
     let txtEmail = document.querySelector('#txtEmail')
-
-    //indexOf -> -1 se elemento não estiver presente; senão 1.
-    if((email.value.indexOf('@') == -1) || (email.value.indexOf('.') == -1)) {
-        txtEmail.innerHTML = 'E-mail inválido'
-        txtEmail.style.color = 'red'
+    if ((email.value.indexOf('@') == -1) || (email.value.indexOf('.') == -1)) {
+        txtEmail.innerHTML = 'Email inválido'
+        txtEmail.style.color = 'pink'
         emailOk = false
     } else {
-        txtEmail.innerHTML = '';
+        txtEmail.innerHTML = ''
         emailOk = true
     }
 }
 
-function validaAssunto(){
-    let txtAssunto = document.querySelector('#txtAssunto')
-    let contCaracteres = assunto.value.length
-
-    txtAssunto.innerHTML = contCaracteres
-
-    if(assunto.value.length > 200) {
-        txtAssunto.style.color = 'red'
-        assuntoOk = false
+function validaFilme() {
+    let txtFilme = document.querySelector('#txtFilme')
+    if(filme.value.length == ''){
+        txtFilme.innerHTML = 'Selecione um filme para votar'
+        txtFilme.style.color = 'pink'
+        filmeOk = false
     } else {
-        txtAssunto.style.color = 'black'
-        assuntoOk = true
+        txtFilme.innerHTML = ''
+        filmeOk = true
     }
+
 }
 
 function enviar() {
-    if (nomeOk && emailOk && assuntoOk) {
-        alert('Formulário enviado!')
+    if (nomeOk && emailOk && filmeOk) {
+        let voto = filme.value
+        alert('Voto em ' + voto + ' computado com sucesso!')
     } else {
         alert('Preencha o formulário corretamente.')
     }
-}
-
-function mapaZoom() {
-    mapa.style.width = '550px'
-    mapa.style.height = '300px'
-}
-
-function mapaNormal() {
-    mapa.style.width = '400px'
-    mapa.style.height = '250px'
 }
